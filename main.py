2eb2d558b5e0fe7b05ee8b3d4a2f641bdd1c0da7
Kirill Novikov <novikovk2003@yandex.ru>
2024-03-18 14:33:47 +0300

    update energy

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 import pygame
-from math import sin, cos
+from math import sin, cos, radians, sqrt
 
 
-RGB_BLUE = (0, 0, 255)
-RGB_ORANGE = (255, 165, 0)
+RGB_BLUE = (0, 0, 225)
+RGB_ORANGE = (225, 125, 0)
 RGB_DARKGREY = (63, 63, 63)
 RGB_LIGHTGREY = (155, 155, 155)
+RGB_WHITE = (255, 255, 255)
+RGB_BLACK = (0, 0, 0)
 
 screenWidth = 1000
 screenHeight = 600
 menuHeight = 100
 
-class Circle:
-    def __init__(self, screen, type=0, x=50, y=(screenHeight-menuHeight)/2, radius=40):
+class Missile:
+    def __init__(self, screen, type = 0, x = 50, y = (screenHeight - menuHeight) / 2 + menuHeight, radius=40):
         self.screen = screen
         self.type = type
         self.x = x
@@ -26,14 +28,17 @@ class Circle:
         else:
             self.color = RGB_LIGHTGREY
 
-    def setSpeedTange(self, speed, ange):
-        self.speed = speed
+    def setSpeedTange(self, energy, ange):
+        self.energy = energy
         self.ange = ange
 
     def move(self):
-        if (self.speed):
-            self.x += self.speed * cos(self.ange)
-            self.y += self.speed * sin(self.ange)
+        if (self.energy):
+            self.x += sqrt(2 * self.energy) * cos(radians(self.ange))
+            self.y += sqrt(2 * self.energy) * sin(radians(self.ange))
+            self.energy -= 0.06
+            if self.energy < 0:
+                self.energy = 0
 
     def draw(self):
         pygame.draw.circle(self.screen, self.color, (self.x, self.y), self.radius)
@@ -45,16 +50,14 @@ pygame.init()
 screen = pygame.display.set_mode((screenWidth, screenHeight))
 pygame.display.set_caption("Карамболь")
 
-# Параметры круга
-circle_radius = 50
-circle_x = screenWidth // 2
-circle_y = screenHeight // 2
-speed = 5
-
-# Переменные для подсчета FPS
+#ФПС
 clock = pygame.time.Clock()
 font = pygame.font.Font(None, 36)
 fps = 0
+
+#Круг
+missile = Missile(screen, 2)
+missile.setSpeedTange(12.5, 5)
 
 # Основной игровой цикл
 running = True
@@ -63,20 +66,15 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    screen.fill((255, 255, 255))
+    screen.fill(RGB_WHITE)
     pygame.draw.rect(screen, RGB_LIGHTGREY, (0, 0, screenWidth, menuHeight))
 
-    # Движение круга
-    circle_x += speed
-    if circle_x > screenWidth + circle_radius:
-        circle_x = -circle_radius
-
-    # Отрисовка круга
-    pygame.draw.circle(screen, RGB_BLUE, (circle_x, circle_y), circle_radius)
+    missile.move()
+    missile.draw()
 
     # Подсчет FPS
     fps = clock.get_fps()
-    fps_text = font.render(f"FPS: {int(fps)}", True, (0, 0, 0))
+    fps_text = font.render(f"FPS: {int(fps)}", True, RGB_BLACK)
     screen.blit(fps_text, (10, 10))
 
     pygame.display.flip()
